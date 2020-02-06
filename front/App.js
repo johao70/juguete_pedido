7f133032074d9b9e305625dab6239b74f9bf9d42
johao70 <perlazajohao@gmail.com>
2020-02-05 20:13:22 -0500

    finalizado

--- front/App.js
+++ front/App.js
@@ -1,31 +1,52 @@
-import React, { useState } from 'react';
-import { Picker, View, Image, StyleSheet, Button, Alert, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Picker, View, Image, StyleSheet, Button, Text, Alert } from 'react-native';
+import axios from 'axios';
 
+const URL = "http://192.168.1.8:5000/api/";
 
-export default function App() {
-  const [selectedValue, setSelectedValue] = useState('java');
+export default class App extends Component {
+  state = {
+    response: ''
+  }
 
-  return (
-    <View style={styles.container}>
-      <Text style={{width: 150, height: 50, fontSize: 20, fontWeight: 'bold'}}> Juguete Pedidos</Text>
-      <Image
-          style={{width: 200, height: 200}}
-          source={require('./assets/silla.jpg')}
+  handleSelect = text => {
+    this.setState({ response: text });
+  };
+
+  getData = () => {
+    axios.get(URL+this.state.response)
+    .then(response => {
+      Alert.alert("Usted ha selecionado: " + JSON.stringify(response.data.empresa),
+      ("Tiempo estimado de: " + JSON.stringify(response.data.tiempo) + " para su entrega"))
+      // this.setState({ serverRes: response.data.datos })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{width: 150, height: 50, fontSize: 20, fontWeight: 'bold'}}> Juguete Pedidos</Text>
+        <Image
+            style={{width: 200, height: 200}}
+            source={require('./assets/silla.jpg')}
+          />
+        <Picker style={{ height: 150, width: 250 }} onValueChange={this.handleSelect}>
+          <Picker.Item label="" value="" />
+          <Picker.Item label="Fedex" value="fedex" />
+          <Picker.Item label="Correo del Ecuador" value="correos-ecuador" />
+          <Picker.Item label="Servientrega" value="servientrega" />
+        </Picker>
+        <Button
+          title="Procesar"
+          onPress = {() => {this.getData()}}
+          // onPress={() => alert(JSON.stringify(this.state.response))}
         />
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 150, width: 250 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-        <Picker.Item label="Fedex" value="fedex" />
-        <Picker.Item label="Correo del Ecuador" value="correos" />
-        <Picker.Item label="Servientrega" value="servi" />
-      </Picker>
-      <Button
-        title="Procesar"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-    </View>
-  );
+      </View>
+    )
+  }
 }
 
 
